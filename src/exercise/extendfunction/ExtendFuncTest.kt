1f04a3b4b72623562b8fb72231f7f51294161f4c
liyaxi <liyx2018@gmail.com>
2022-02-16 15:37:32 +0800

    扩展函数；

---
+++ src/exercise/extendfunction/ExtendFuncTest.kt
@@ -0,0 +1,8 @@
+package exercise.extendfunction
+
+fun main(args: Array<String>) {
+
+  var str:String = "hello"
+  str.printStr()
+
+}
---
+++ src/exercise/extendfunction/String.kt
@@ -0,0 +1,9 @@
+package exercise.extendfunction
+
+
+/**
+ * 扩展函数 fun ClassName.fucName(){ }
+ */
+fun String.printStr(){
+    println("扩展函数打印出来的：$this")
+}
