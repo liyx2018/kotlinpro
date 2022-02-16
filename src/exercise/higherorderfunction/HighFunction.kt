696c2d3a5457fe1f40b71fd857025cff64765ddb
liyaxi <liyx2018@gmail.com>
2022-02-16 17:30:20 +0800

    高阶函数；

---
+++ src/exercise/higherorderfunction/HighFunction.kt
@@ -0,0 +1,18 @@
+package exercise.higherorderfunction
+
+fun main(args: Array<String>) {
+    val nums =getNum(12,15,::plus)
+    println("两数相加："+nums)
+
+}
+
+fun getNum(num1:Int,num2:Int,fuc:(Int,Int) -> Int):Int{
+    return fuc(num1,num2)
+}
+
+fun plus(num1:Int,num2:Int):Int{
+    return num1+num2
+}
+fun minus(num1:Int,num2:Int):Int{
+    return num1-num2
+}
